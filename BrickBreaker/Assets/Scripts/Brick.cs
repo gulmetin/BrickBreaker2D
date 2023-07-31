58a1e9e1e8a2f727c333bd4d46aaeddc2980329f
gulmetin <gul8metin@gmail.com>
2023-07-31 11:58:07 +0300

    feat(script) : Brick
    Creating Script that involves the ball interacting with the brick and changing their state

---
+++ BrickBreaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer {get; private set;}
+     public int health {get; private set;}
+    public Sprite[] states;
+
+    private void Awake() {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Start() {
+        this.health = this.states.Length;
+        this.spriteRenderer.sprite = this.states[this.health-1];
+    }
+
+    private void  Hit() {
+        this.health--;
+        
+        if(this.health<=0){
+            this.gameObject.SetActive(false);
+        }else{
+            this.spriteRenderer.sprite = this.states[this.health-1];
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.name == "Ball"){
+            Hit();
+        }
+    }
+}

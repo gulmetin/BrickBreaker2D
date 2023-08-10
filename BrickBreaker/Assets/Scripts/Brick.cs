58b977bb32bea3d241e42abe203ae6ba64439efd
gulmetin <gul8metin@gmail.com>
2023-08-10 21:01:14 +0300

    feat(script) : Add Score
    Adding score codes.

--- BrickBreaker/Assets/Scripts/Brick.cs
+++ BrickBreaker/Assets/Scripts/Brick.cs
@@ -5,30 +5,35 @@ using UnityEngine;
 public class Brick : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer {get; private set;}
-     public int health {get; private set;}
+    public int health {get; private set;}
+    public int points =120;
     public Sprite[] states;
+    GameManager _gameManager;
 
     private void Awake() {
         this.spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
     private void Start() {
         this.health = this.states.Length;
         this.spriteRenderer.sprite = this.states[this.health-1];
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     private void  Hit() {
+       
+        _gameManager.Score+= points/health;
         this.health--;
+
         
         if(this.health<=0){
-            this.gameObject.SetActive(false);
+            Destroy(this.gameObject);
         }else{
             this.spriteRenderer.sprite = this.states[this.health-1];
         }
     }
     
     private void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.gameObject.name == "Ball"){
-            Hit();
-        }
+        Hit();
     }
 }

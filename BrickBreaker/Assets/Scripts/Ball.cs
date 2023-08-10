b29e15d00e42bf600352c85a9aef4c7d6c393385
gulmetin <gul8metin@gmail.com>
2023-08-10 21:03:30 +0300

    feat(script) : Update Ball
    Update the number of balls owned and regenerate balls

--- BrickBreaker/Assets/Scripts/Ball.cs
+++ BrickBreaker/Assets/Scripts/Ball.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    float _speed =10f;
+    public float _speed =8f;
     Rigidbody2D _rigidbody;
     Vector2 _velocity;
+    GameManager _gameManager;
+    SpriteRenderer _renderer;
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        Invoke("Launch",1f);
+    }
+
+    private void Launch() {
         _rigidbody.velocity = Vector2.down*_speed;
     }
 
@@ -18,10 +27,15 @@ public class Ball : MonoBehaviour
     {
         _rigidbody.velocity = _rigidbody.velocity.normalized * _speed;
         _velocity = _rigidbody.velocity;
+
+        if(!_renderer.isVisible){
+            _gameManager.Balls--;
+            Destroy(gameObject);
+        }
     }
 
+
     private void OnCollisionEnter2D(Collision2D other) {
-        
         _velocity=Vector2.Reflect(_velocity,other.contacts[0].normal);
         _rigidbody.velocity = _velocity*_speed;
     }
